22cd29ceb47ddbd99c5d9d95c72c65f022335496
Tessa <tess.starkweather@gmail.com>
2023-02-07 21:02:11 -0500

    about section and contact form bare bones

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import { BsGithub } from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
 
 function About() {
     return (
-        <h1>About</h1>
+        <div>
+            <div className="title-big pb-3 mb-3">
+                <h3>ABOUT ME</h3>
+            </div>
+            <p>
+                Hi, my name is Tessa.
+            </p>
+            <p> I am a recent graduate from UNC Chapel
+                Hill with a Bachelor of Science in Biology. I am currently employed as an air
+                quality
+                analyst, but my
+                newly discovered passion for computer science has led me to UNC's Coding Bootcamp.
+            </p>
+            <div>
+                <a href="https://github.com/t-starkw"><i><BsGithub /></i></a>
+                <a href="https://www.linkedin.com/in/tessa-starkweather-b61941200/"><i><BsLinkedin /></i></a>
+                <a href="https://www.instagram.com/tessa.starkweather/"><i><BsInstagram /></i></a>
+            </div>
+        </div>
+
     );
 }
 
