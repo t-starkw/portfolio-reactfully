3f6b7f8a02d9d98a55a42a742e26765a85ce011c
Tessa <tess.starkweather@gmail.com>
2023-02-09 14:12:27 -0500

    portfolio pages renders project cards

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
+// project function generates indivual cards to be used in the portfolio section
 
-function Project() {
+function Project(props) {
+
     return (
-        <h1>Project</h1>
+        <Container>
+
+            <Row md={3} >
+                {props.projects.map((project) => (
+                    <Col>
+                        <Card className="bg-dark text-white">
+                            <Card.Img src={project.image} alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title key={project.id}>{project.title}</Card.Title>
+                                <Card.Text>
+                                    {project.description}
+                                </Card.Text>
+                                <Card.Subtitle className="mb-2 ">Languages: {project.languages}</Card.Subtitle>
+                                <Card.Link href={project.repo}>View Repo</Card.Link>
+                                <Card.Link href={project.live}>View Site</Card.Link>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+
+        </Container>
     );
 }
 
