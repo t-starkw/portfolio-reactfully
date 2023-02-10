ca8aba9227f969006bc81e996df2c071d2d9efe7
Tessa <tess.starkweather@gmail.com>
2023-02-09 20:49:52 -0500

    format contact form and add modal

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,66 +1,78 @@
 import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 function Contact() {
 
+    const [alert, showAlert] = useState(false);
+    const handleClose = () => showAlert(false);
+    const handleShow = () => showAlert(true);
+
     const [submitted, setSubmitted] = useState(false);
     const handleSubmit = () => {
-        setTimeout(() => {
-            setSubmitted(true);
-        }, 100);
+        setSubmitted(true);
+        handleShow()
     };
 
     if (submitted) {
         return (
             <>
-                <div className="text-2xl">Thank you!</div>
-                <div className="text-md">We'll be in touch soon.</div>
+                <Modal className='text-center' show={alert} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title className='w-100'>Success!🚀</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Thanks for reaching out!</Modal.Body>
+                    <Modal.Body>We will be in touch soon.</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </>
         );
     }
 
     return (
-        <Container>
-            <Row className='d-flex justify-content-center'>
-                <Col md='6' className='bg-default mt-5'>
-                    <form className='text-center' onSubmit={handleSubmit}>
-                        <h1>Contact Me!</h1>
-                        <div className=" mb-3 pt-0">
-                            <input
-                                type="text"
-                                placeholder="Your name"
-                                name="name"
-                                required
-                            />
-                        </div>
-                        <div className="mb-3 pt-0">
-                            <input
-                                type="email"
-                                placeholder="Email"
-                                name="email"
-                                required
-                            />
-                        </div>
-                        <div className="mb-3 pt-0">
-                            <textarea
-                                placeholder="Your message"
-                                name="message"
-                                required
-                                rows='5'
-                            />
-                        </div>
-                        <div className="mb-3 pt-0">
-                            <button type="submit">
-                                Send a message
-                            </button>
-                        </div>
-                    </form>
-                </Col>
-            </Row>
-        </Container>
+        <div>
+            <form className='text-center' onSubmit={handleSubmit}>
+                <h1 className='text-center mb-3'>Contact Me!</h1>
+                <div className=" mb-3 pt-0">
+                    <input
+                        className='feedback-input'
+                        type="text"
+                        placeholder="Your name"
+                        name="name"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <input
+                        className='feedback-input'
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <textarea
+                        className='feedback-input'
+                        placeholder="Your message"
+                        name="message"
+                        required
+                        rows='5'
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </div>
+            </form>
+
+        </div>
+
     );
 }
 
